77da5c8fa1dc8c1a196a0755f98f80edf1deafc3
Emiliano Lafuente <emilianolafuente@hotmail.com>
2023-04-13 22:19:38 -0300

    chore: estructura inicial y refacorizar 2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
+const { port } = require('./config');
 const app = require('./index')
 
-const port = 3000
-
-app.listen(3000, () => {
-    console.log("Server running at port 3000");
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
 })
