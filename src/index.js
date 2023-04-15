07799ec5ab1378f9a61af7be3531f354ca98649f
Emiliano Lafuente <emilianolafuente@hotmail.com>
2023-04-15 13:38:53 -0300

    conextion to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express= require ('express')
 const router = require('./router')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(express.json())
 
 router(app)
+
+
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.nbnuujb.mongodb.net/?retryWrites=true&w=majority');
+
+mongoose.connection.once('open', () => {
+    console.log('Successfully connected to DB');
+  });
+
+mongoose.connection.on('error', error => {
+    console.error('Error connecting to DB', error);
+  });
 
 
 module.exports = app
